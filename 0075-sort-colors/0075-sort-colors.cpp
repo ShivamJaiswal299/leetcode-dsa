7bb405d722681da322e088b2ca43ff8a38821cb5
Shivam Jaiswal <Shivamjaiswalvg2011@gmail.com>
2026-08-18 16:51:13 +0530

    Time: 0 ms (100%), Space: 11.6 MB (79.39%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void sortColors(vector<int>& a) {
+      //DOING BY DUTCH NATIONAL FLAG ALGORITHM
+      int low = 0, mid = 0, high = a.size()-1;
+      while(mid<=high){
+        if(a[mid]==0) swap(a[low++],a[mid++]);
+        else if(a[mid]==1) mid++;
+        else if(a[mid]==2) swap(a[mid],a[high--]);
+      }
+        
+    }
+};
