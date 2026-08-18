dec13b23c5e3ce1f4c031915451cef1a8af6d4ab
Shivam Jaiswal <Shivamjaiswalvg2011@gmail.com>
2026-08-18 17:04:04 +0530

    Time: 2 ms (50.18%), Space: 8.7 MB (36.33%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    // bool isPalindrome(int x) {
-    //   if(x<0) return false;
-    //   if(x<10) return true;
-    //   int digit=log10(x)+1;
-    //   int front,back;
-    //   for(int i=1;i<=digit/2;i++){
-    //     back=(x/(int)pow(10,i-1))%10;
-    //     front=(x/(int)pow(10,digit-i))%10;
-    //     if(front!=back) return false;
-    //   }
-    //   return true;
-    // }
     bool isPalindrome(int x) {
-        string str = to_string(x);
-        reverse(str.begin(),str.end());
-        return str==to_string(x);
+      if(x<0) return false;
+      if(x<10) return true;
+      int digit=log10(x)+1;
+      int front,back;
+      for(int i=1;i<=digit/2;i++){
+        back=(x/(int)pow(10,i-1))%10;
+        front=(x/(int)pow(10,digit-i))%10;
+        if(front!=back) return false;
+      }
+      return true;
     }
+    // bool isPalindrome(int x) {
+    //     string str = to_string(x);
+    //     reverse(str.begin(),str.end());
+    //     return str==to_string(x);
+    // }
 };
