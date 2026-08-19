e636223b873495fc3e04c7ea57070b834428728e
Shivam Jaiswal <Shivamjaiswalvg2011@gmail.com>
2026-08-19 19:50:04 +0530

    Time: 0 ms (100%), Space: 13.5 MB (78.43%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int searchInsert(vector<int>& a, int x) {
-        //binary search
         int low = 0 , high = a.size()-1;
-        int minIdx=a.size();
+        int idx=a.size();
         while(low<=high){
             int mid = low + (high-low)/2;
-            // if target is low we keep searching , if target==a[mid] we keep searching that also cuz duplicate can exists
+            //here we will find lower bound i.e. equal or just next one
             if(a[mid]==x || a[mid]>x)  {
                 high=mid-1;
-                minIdx=mid;
+                idx=mid;
             }
             else if(a[mid]<x) low=mid+1;
         }
-        return minIdx;
+        return idx;
     }
 };
