b310ea27d8271928f6964d3ce393d90ad9667c2b
Shivam Jaiswal <Shivamjaiswalvg2011@gmail.com>
2026-08-21 18:55:53 +0530

    Time: 0 ms (100%), Space: 26.2 MB (36.65%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -4,6 +4,7 @@ public:
       //xor method will give us time O(n) but we want O(log n) so not using it.
       int n=a.size();
       int low=0,mid,high=n-1;
+      //the logic is - a new pair will always start from even index, if the pair is starting at odd index, then something is wrong before or at it.
       while(low<high){
         mid = low + (high - low)/2;
         if(mid%2==0){
@@ -15,6 +16,6 @@ public:
           else low=mid+1;
         }
       }
-      return a[high];
+      return a[low];//high or low , anything can work as loop stops when low==high.
     }
     };
