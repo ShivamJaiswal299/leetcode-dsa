6422003087787dafccfb1d9b7b7aa5d5d1cdedd6
Shivam Jaiswal <Shivamjaiswalvg2011@gmail.com>
2026-08-21 12:52:35 +0530

    Time: 0 ms (100%), Space: 14.1 MB (80.2%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -6,9 +6,9 @@ public:
         int low = 0, high = n-1;
         while(low<high){
           mid = low + (high - low)/2;
-          if(a[mid]<=a[high]) high=mid;
-          else if(a[mid]>a[high])low=mid+1;
+          if(a[mid]<=a[high]) high=mid; //this means min will be low->mid
+          else if(a[mid]>a[high])low=mid+1;//this means min will be mid+1->high
         }
-        return a[high];
+        return a[low];//here a[low] or a[mid] both works as the loop will only stop when high == low , and mid cant be cuz mid will be calculated after high and low are calculated and that too in the next iteration and the next(last) iteration will never.
     }
 };
