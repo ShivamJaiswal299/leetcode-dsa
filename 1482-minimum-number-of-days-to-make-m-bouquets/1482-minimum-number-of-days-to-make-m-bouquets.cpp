dfb76b04754bc469b1f47576e394989d7c25ccbf
Shivam Jaiswal <Shivamjaiswalvg2011@gmail.com>
2026-08-23 18:33:21 +0530

    Time: 29 ms (17.04%), Space: 70.6 MB (12.41%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -2,7 +2,9 @@ class Solution {
 public:
     int minDays(vector<int>& a, int m, int k) {
         int maxBloomday=1,n=a.size();
+        if(n<(long long)m*k) return -1;//if total flowers are less then that are req, then its not possible
         for(int i=0;i<n;i++) maxBloomday=max(maxBloomday,a[i]);
+        //binary search tech
         int low = 1,mid,high = maxBloomday,minDay=maxBloomday+1;
         while(low<=high){
           mid=low + (high-low)/2;
@@ -12,11 +14,13 @@ public:
           for(int i=0;i<n;i++){
             if(currDay>=a[i]) tempReqFlower--;
             else tempReqFlower=k;
+            //now 1 bouquet is made
             if(tempReqFlower==0){
               tempReqBouquet--;
               tempReqFlower=k;
             }
           }
+          // checking that all bouquet are made or not
             if(tempReqBouquet<=0){
               minDay=min(minDay,currDay);
               high=mid-1;
@@ -24,8 +28,8 @@ public:
             else{
               low=mid+1;
             }
-
         }
+        //-1 if not possible
         return minDay==maxBloomday+1?-1:minDay;
     }
 };
