79941dafba6cb3305013efd1b06a6522235cf054
Shivam Jaiswal <Shivamjaiswalvg2011@gmail.com>
2026-08-23 16:10:18 +0530

    Time: 4 ms (86.92%), Space: 22.8 MB (99.51%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -12,7 +12,7 @@ public:
           //checking wheather this k(i.e.mid) can possibly be answer or not 
           int i,tempH;
           for(i=0,tempH=h;(tempH>-1) && i<n;i++) tempH=tempH-(a[i]%k==0?a[i]/k:a[i]/k+1);
-          //IMP NOTE - if k=6 , h=14 12 12 12 12 12 12 6 -> h=13 this means he gets free 1 hr before so u will think lets decrease k but if u do k=5 , then h req. will be higher , so its not neccessay that remaining h should become completely zero for finding k ,  it can be 1, 2, 3, etc 
+          //IMP NOTE- if k=6 , h=14 12 12 12 12 12 12 6 -> h=13 this means he gets free 1 hr before so u will think lets decrease k but if u do k=5 , then h req. will be higher , so its not neccessay that remaining h should become completely zero for finding k ,  it can be 1, 2, 3, etc 
           //also keep in mind if some k is giving temph==0 then its not neccesary that it is the minimun k that can do so eg- 26 26 26 26 26 26 26 here h=26 gives tempH=0 but h=25 too give that.
           if(tempH<0) low=mid+1; 
           else if(tempH>=0){
