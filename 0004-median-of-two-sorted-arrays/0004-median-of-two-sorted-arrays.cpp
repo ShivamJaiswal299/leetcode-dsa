d26a6e4b48ad4ca6434cdd6244fe464cdbeab38f
Shivam Jaiswal <Shivamjaiswalvg2011@gmail.com>
2026-09-01 16:44:41 +0530

    Time: 4 ms (18.91%), Space: 95.1 MB (61.31%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+//toughest so far
     double findMedianSortedArrays(vector<int>& a1, vector<int>& a2) {
         if (a1.size() > a2.size()) swap(a1, a2);
         int n1 = a1.size(), n2 = a2.size();
@@ -9,7 +10,7 @@ public:
         while (low <= high) {
             int mid1 = low + (high - low) / 2;  
             int mid2 = lefthalf - mid1;        
-
+//HERE IS THE IMP PART - assigning left and right half var: the int_min int_max sht is cuz of the edge cases where all the elements in the left half are from a single array, also u may see, mid1 and mid2 are not index , they are sizes, thatswhy instead of mid1(left) it is mid1-1 and mid+1(right) it is just mid
             int a1left  = (mid1 > 0)  ? a1[mid1 - 1] : INT_MIN;
             int a1right = (mid1 < n1) ? a1[mid1]     : INT_MAX;
             int a2left  = (mid2 > 0)  ? a2[mid2 - 1] : INT_MIN;
@@ -27,6 +28,6 @@ public:
                 low = mid1 + 1;
             }
         }
-        return -1;
+        return -1;//wont reach here!!
     }
 };
