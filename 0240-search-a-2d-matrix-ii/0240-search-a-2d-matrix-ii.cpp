93411874ba7393955e57953b11fe52fe9918568c
Shivam Jaiswal <Shivamjaiswalvg2011@gmail.com>
2026-09-08 18:03:32 +0530

    Time: 51 ms (67.89%), Space: 18.7 MB (69.24%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -5,8 +5,8 @@ public:
       int cIdx=col-1,rIdx=0;//as we will start from right corner
       while(rIdx<row && cIdx>=0){//stops when boundary hits
         if(mat[rIdx][cIdx]==x) return true;
-        else if(mat[rIdx][cIdx]<x) rIdx++;
-        else if(mat[rIdx][cIdx]>x) cIdx--;
+        else if(mat[rIdx][cIdx]<x) rIdx++;//rowidx increasing as current row wont have the element
+        else if(mat[rIdx][cIdx]>x) cIdx--;//columnidx increasing as currnt column wont have the element
       }
       return false;
     }
