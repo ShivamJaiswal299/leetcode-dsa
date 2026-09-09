d52c11a9164cc2768f9df18f57ec46ca6c04244d
Shivam Jaiswal <Shivamjaiswalvg2011@gmail.com>
2026-09-09 23:32:27 +0530

    Time: 0 ms (100%), Space: 9.7 MB (48.82%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
   bool isAnagram(string s, string t) {
+    if(s.length()!=t.length()) return false;
+    //make a array to store freq for all char
     int freq[26]={0};
-    for(char c: s) freq[c-'a']++;
-    for(char c: t) freq[c-'a']--;
-    for(int i=0;i<26;i++) if (freq[i]) return false;
+    for(char c: s) freq[c-'a']++;//++ for 1st array elements
+    for(char c: t) freq[c-'a']--;//-- for 2nd array elements
+    for(int i=0;i<26;i++) if (freq[i]) return false; // if not zero then its not anagram
     return true;
   }
 };
