7c4bd43e198336e0581e19ecad0346d30c2b5d95
Shivam Jaiswal <Shivamjaiswalvg2011@gmail.com>
2026-09-09 16:44:40 +0530

    Time: 4 ms (20.85%), Space: 10 MB (88.05%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,20 +1,44 @@
+// class Solution {
+// public:
+//     string reverseWords(string s) {
+//       string word = " " ,result = ""; 
+//         for(int i = s.length()-1;i>=0;i--){
+//           char c = s[i];
+//           if(c==' ' && word!=" "){
+//             result+=word;
+//             word=" ";
+//           }
+//           else if (c!=' '){
+//             word.insert(word.begin(),c);
+
+//           }
+//         }
+//       if(word!=" ") result+=word;
+//       result.pop_back();
+//       return result;
+//     }
+// };
+//better time complexity (as not inserting at front everytime which takes O(n))
 class Solution {
 public:
     string reverseWords(string s) {
-      string word = " " ,result = ""; 
-        for(int i = s.length()-1;i>=0;i--){
-          char c = s[i];
-          if(c==' ' && word!=" "){
-            result+=word;
-            word=" ";
-          }
-          else if (c!=' '){
-            word.insert(word.begin(),c);
-
-          }
+        string word, result;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char c = s[i];
+            if (c == ' ' && !word.empty()) {
+                reverse(word.begin(), word.end());//reversing once instead of inserting everytime at front. eg - "yks"
+                result += word + " "; //"sky" becomes "sky "
+                word="";
+            } else if (c != ' ') {
+                word.push_back(c); 
+            }
         }
-      if(word!=" ") result+=word;
-      result.pop_back();
-      return result;
+        if (!word.empty()) {
+            reverse(word.begin(), word.end());
+            result += word;
+        } else if (!result.empty()) {
+            result.pop_back();  // remove trailing space
+        }
+        return result;
     }
 };
